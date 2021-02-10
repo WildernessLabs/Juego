cc637f7208421b3463b4678c02ab19f66e44775b
Adrian Stevens <adrian.stevens@gmail.com>
2021-02-09 23:14:27 -0800

    Add Options menu (partially working)

--- Source/Juego/Juego/MeadowApp.cs
+++ Source/Juego/Juego/MeadowApp.cs
@@ -23,6 +23,8 @@ namespace Juego
 
         IGame currentGame;
 
+        const string version = "0.4.1";
+
         public MeadowApp()
         {
             Initialize();
@@ -54,15 +56,16 @@ namespace Juego
             hardware.Left.Clicked += Left_Clicked;
             hardware.Right.Clicked += Right_Clicked;
             hardware.Down.Clicked += Down_Clicked;
-            hardware.Select.Clicked += Select_Clicked;
-            hardware.Start.Clicked += Start_Clicked;
+
+            if (hardware.Select != null) hardware.Select.Clicked += Select_Clicked;
+            if (hardware.Start != null) hardware.Start.Clicked += Start_Clicked;
         }
 
         void DrawSplashScreen(GraphicsLibrary graphics)
         {
             graphics.Clear();
             graphics.DrawRectangle(0, 0, hardware.Graphics.Width, hardware.Graphics.Height);
-            graphics.DrawText(hardware.Graphics.Width / 2, hardware.Graphics.Height / 3, "Juego v0.4", GraphicsLibrary.ScaleFactor.X1, GraphicsLibrary.TextAlignment.Center);
+            graphics.DrawText(hardware.Graphics.Width / 2, hardware.Graphics.Height / 3, $"Juego v{version}", GraphicsLibrary.ScaleFactor.X1, GraphicsLibrary.TextAlignment.Center);
             graphics.Show();
         }
 
@@ -96,7 +99,14 @@ namespace Juego
 
         private void Left_Clicked(object sender, EventArgs e)
         {
-            currentGame?.Left();
+            if (menu.IsEnabled)
+            {
+             //   menu.Back();
+            }
+            else
+            {
+                currentGame?.Left();
+            }
         }
 
         private void Up_Clicked(object sender, EventArgs e)
@@ -192,11 +202,22 @@ namespace Juego
                 new MenuItem("Span4", command: "startSpan4"),
                 new MenuItem("Snake", command: "startSnake"),
                 new MenuItem("Tetraminos", command: "startTetraminos"),
+                new MenuItem("Options", 
+                    subItems: new MenuItem[]{new MenuItem("Sound {value}", id: "sound", type: "OnOff", value: true),
+                                             new MenuItem("Volume {value}", id: "volume", type: "Numerical", value: 5),
+                                             new MenuItem("Clear scores", command: "clearScores"),
+                                             new MenuItem($"Version {version}") }),
             };
 
             menu = new Menu(display, menuItems);
 
             menu.Selected += Menu_Selected;
+            menu.Exited += Menu_Exited;
+        }
+
+        private void Menu_Exited(object sender, EventArgs e)
+        {
+            Console.WriteLine("Menu exited");
         }
 
         private void Menu_Selected(object sender, MenuSelectedEventArgs e)
