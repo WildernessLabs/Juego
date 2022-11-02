4171a34a9f4ee40d9052cd7d48f41f4bf392e1f8
bryan costanich <b@wowzer.net>
2022-11-02 11:43:30 -0700

    Initial v2 app work

---
+++ Source/Software/Juego.Core/JuegoHardware_v2.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Meadow.Foundation.Audio;
+using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Leds;
+using Meadow.Foundation.Sensors.Buttons;
+using Meadow.Foundation.Sensors.Hid;
+using Meadow.Hardware;
+using Meadow.Foundation.ICs.IOExpanders;
+using Meadow.Gateways.Bluetooth;
+using Meadow.Devices;
+using Meadow.Units;
+
+namespace Juego.Core
+{
+    public class JuegoHardware_v2
+    {
+        protected F7CoreComputeV2 Device { get; }
+        protected IDigitalInputPort McpInterrupt_1 { get; }
+
+        IGraphicsDisplay Display { get; }
+
+        //==== Comms Busses
+        protected II2cBus? I2c { get; }
+        protected ISpiBus Spi { get; }
+
+        //==== MCP IO Expanders
+        public Mcp23008 Mcp_1 { get; protected set; }
+        public Mcp23008 Mcp_2 { get; protected set; }
+        public Mcp23008 Mcp_VersionInfo { get; protected set; }
+
+        //AnalogJoystick AnalogJoystick { get; }
+
+        //==== Right side buttons
+        public PushButton Right_UpButton { get; protected set; }
+        public PushButton Right_DownButton { get; protected set; }
+        public PushButton Right_LeftButton { get; protected set; }
+        public PushButton Right_RightButton { get; protected set; }
+
+        //==== Left side buttons
+        public PushButton Left_UpButton { get; protected set; }
+        public PushButton Left_DownButton { get; protected set; }
+        public PushButton Left_LeftButton { get; protected set; }
+        public PushButton Left_RightButton { get; protected set; }
+
+        //==== Start/Select Buttons
+        public PushButton StartButton { get; protected set; }
+        public PushButton SelectButton { get; protected set; }
+
+        //==== Speakers
+        public PiezoSpeaker LeftSpeaker { get; protected set; }
+        public PiezoSpeaker RightSpeaker { get; protected set; }
+
+        public JuegoHardware_v2(F7CoreComputeV2 device)
+        {
+            this.Device = device;
+
+            Console.WriteLine("Initialize hardware...");
+
+            //==== I2C Bus
+            Console.WriteLine("Initializing I2C Bus.");
+            try
+            {
+                I2c = Device.CreateI2cBus();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Err initializing I2C Bus: {e.Message}");
+            }
+            Console.WriteLine("I2C initialized.");
+
+            //==== MCPs
+            try
+            {
+                //McpInterrupt_1 = Device.CreateDigitalInputPort(Device.Pins.D09, InterruptMode.EdgeRising);
+                //Mcp_1 = new Mcp23008(I2c, 0x20, McpInterrupt_1);
+                Mcp_1 = new Mcp23008(I2c, 0x20);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Err MCP 1: {e.Message}");
+            }
+            try
+            {
+                //McpInterrupt_2 = Device.CreateDigitalInputPort(Device.Pins.D10, InterruptMode.EdgeRising);
+                //Mcp_2 = new Mcp23008(I2c, 0x20, McpInterrupt_2);
+                Mcp_2 = new Mcp23008(I2c, 0x21);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Err MCP 2: {e.Message}");
+            }
+            try
+            {
+                Mcp_VersionInfo = new Mcp23008(I2c, 0x26);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Err MCP 3: {e.Message}");
+            }
+
+            //==== Speakers
+            try {
+                LeftSpeaker = new PiezoSpeaker(device, device.Pins.D12);
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Err Left Speaker: {e.Message}");
+            }
+            try
+            {
+                LeftSpeaker = new PiezoSpeaker(device, device.Pins.D13);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Err Left Speaker: {e.Message}");
+            }
+
+
+            //==== SPI
+            Console.WriteLine("Initializing SPI Bus.");
+            try
+            {
+                Spi = Device.CreateSpiBus(new Frequency(48, Frequency.UnitType.Kilohertz));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Err initializing SPI: {e.Message}");
+            }
+            Console.WriteLine("SPI initialized.");
+        }
+    }
+}
---
+++ Source/Software/Juego_Demo/MeadowApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using Juego.Core;
+using Meadow.Units;
+
+namespace MeadowApp
+{
+	// Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+	public class MeadowApp : App<F7CoreComputeV2>
+	{
+        JuegoHardware_v2 Hardware { get; set; }
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("App.Initialize()");
+
+            Hardware = new JuegoHardware_v2(Device);
+
+            return base.Initialize();
+        }
+
+        public async override Task Run()
+        {
+            Console.WriteLine("Run.");
+
+            for (int i = 0; i < 5; i++) {
+                await Hardware.LeftSpeaker.PlayTone(new Frequency(440), TimeSpan.FromMilliseconds(500));
+            }
+
+            return;
+        }
+    }
+}
