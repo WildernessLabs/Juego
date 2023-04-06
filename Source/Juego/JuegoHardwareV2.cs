7234f30171ede5f34c4f1e6c354bd48d1d63558b
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-04-05 23:33:39 -0700

    Minor cleanup

--- Source/Juego/JuegoHardwareV2.cs
+++ Source/Juego/JuegoHardwareV2.cs
@@ -24,32 +24,26 @@ namespace WildernessLabs.Hardware.Juego
 
         public IDigitalOutputPort DisplayBacklightPort { get; }
 
-        //==== Comms Busses
         protected II2cBus I2cBus { get; }
         protected ISpiBus Spi { get; }
 
-        //==== MCP IO Expanders
         public Mcp23008 Mcp_1 { get; protected set; }
         public Mcp23008 Mcp_2 { get; protected set; }
         public Mcp23008 Mcp_VersionInfo { get; protected set; }
 
-        //==== Right side buttons
         public PushButton? Right_UpButton { get; protected set; }
         public PushButton? Right_DownButton { get; protected set; }
         public PushButton? Right_LeftButton { get; protected set; }
         public PushButton? Right_RightButton { get; protected set; }
 
-        //==== Left side buttons
         public PushButton? Left_UpButton { get; protected set; }
         public PushButton? Left_DownButton { get; protected set; }
         public PushButton? Left_LeftButton { get; protected set; }
         public PushButton? Left_RightButton { get; protected set; }
 
-        //==== Start/Select Buttons
         public PushButton? StartButton { get; protected set; }
         public PushButton? SelectButton { get; protected set; }
 
-        //==== Speakers
         public PiezoSpeaker? LeftSpeaker { get; protected set; }
         public PiezoSpeaker? RightSpeaker { get; protected set; }
 
@@ -59,18 +53,16 @@ namespace WildernessLabs.Hardware.Juego
 
             Resolver.Log.Info("Initialize hardware...");
 
-            Resolver.Log.Info("Initializing I2C Bus");
             try
             {
                 I2cBus = Device.CreateI2cBus();
+                Resolver.Log.Info("I2C initialized");
             }
             catch (Exception e)
             {
                 Resolver.Log.Error($"Err initializing I2C Bus: {e.Message}");
             }
-            Resolver.Log.Info("I2C initialized");
 
-            //==== MCPs
             try
             {
                 Mcp_Reset = Device.CreateDigitalOutputPort(Device.Pins.D11, true);
@@ -104,7 +96,6 @@ namespace WildernessLabs.Hardware.Juego
                 Resolver.Log.Error($"Err MCP 3: {e.Message}");
             }
 
-            //==== Speakers
             try
             {
                 LeftSpeaker = new PiezoSpeaker(device.Pins.D12);
@@ -122,8 +113,6 @@ namespace WildernessLabs.Hardware.Juego
                 Resolver.Log.Error($"Err Right Speaker: {e.Message}");
             }
 
-            //==== SPI
-            Resolver.Log.Info("Initializing SPI Bus");
             try
             {
                 var config = new SpiClockConfiguration(new Frequency(48000, Frequency.UnitType.Kilohertz), SpiClockConfiguration.Mode.Mode0);
@@ -135,7 +124,6 @@ namespace WildernessLabs.Hardware.Juego
             }
             Resolver.Log.Info("SPI initialized");
 
-            //==== Display
             if (Mcp_1 != null)
             {
                 DisplayBacklightPort = Device.CreateDigitalOutputPort(Device.Pins.D05, true);
@@ -156,7 +144,6 @@ namespace WildernessLabs.Hardware.Juego
                 Resolver.Log.Info("Display initialized");
             }
 
-            //==== Buttons
             if (Mcp_1 != null)
             {
                 var upPort = Mcp_1.CreateDigitalInputPort(Mcp_1.Pins.GP1, InterruptMode.EdgeBoth, ResistorMode.InternalPullUp);
@@ -165,11 +152,8 @@ namespace WildernessLabs.Hardware.Juego
                 var leftPort = Mcp_1.CreateDigitalInputPort(Mcp_1.Pins.GP4, InterruptMode.EdgeBoth, ResistorMode.InternalPullUp);
 
                 Left_UpButton = new PushButton(upPort);
-
                 Left_RightButton = new PushButton(rightPort);
-
                 Left_DownButton = new PushButton(downPort);
-
                 Left_LeftButton = new PushButton(leftPort);
             }
 
